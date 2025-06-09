fc82ec4190152d60d362ee3bc8c5d5cdedaa3df5
tariba <tariqxa@gmail.com>
2025-06-09 16:56:39 +0100

    added try-catch for error handling in routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,22 +6,40 @@ const router = express.Router();
  * a fn that fetch all data
  */
 router.get("/", async function (req, res) {
+  try {
   const data = await getAllQuestions();
   res.status(200).json({
     success: true,
     payload: data.rows,
   });
+} catch (error) {
+  console.error('Error fetching all questions', error);
+  res.status(500).json ({
+    success: false,
+    message: 'Failed to fetch questions',
+    error: error.message
+  })
+}
 });
 
 /*
  * a fn that fetch data based on ID (didn't used it)
  */
 router.get("/:id", async function (req, res) {
+  try {
   const data = await getQuestionsById(req.params.id);
   res.status(200).json({
     success: true,
     payload: data.rows,
   });
+} catch (error) {
+  console.error(`Error fetching questions with ID ${req.params.id}:`, error );
+  res.status(500).json({
+    success: false,
+    message: 'Failed to fetch question by ID',
+    error: error.message,
+  })
+};
 });
 
 export default router;
